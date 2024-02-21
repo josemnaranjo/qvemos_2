45a620dec15f0606e8858bfc321fd83dade998d6
Josemaria Naranjo <josemnaranjoc@gmail.com>
2024-02-21 12:01:04 -0300

    pagina de votacion

---
+++ client/src/Components/VoteForm.jsx
@@ -0,0 +1,14 @@
+const VoteForm = () => {
+  return (
+    <main className="flex flex-col justify-center items-center">
+      <h1 className=" mt-[10px] w-[342px] h-[62px] font-grotesk font-bold text-lg text-tertiary text-center">
+        ¡Que comiencen los juegos!
+      </h1>
+      <p className="mt-[28px] w-[342px] font-grotesk font-light text-body text-center text-secondary">
+        Evalúa las recomendaciones del 1 al 7
+      </p>
+    </main>
+  );
+};
+
+export default VoteForm;
--- client/src/Views/Votacion.jsx
+++ client/src/Views/Votacion.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import WaitingPage from "../Components/WaitingPage";
+import VoteForm from "../Components/VoteForm";
 
 const Votacion = () => {
   const [anfi, setAnfi] = useState(false);
 
-  function VoteForm() {
-    return <h1> Formulario de votación</h1>;
-  }
   return <>{anfi ? <VoteForm /> : <WaitingPage setAnfi={setAnfi} />}</>;
 };
 
