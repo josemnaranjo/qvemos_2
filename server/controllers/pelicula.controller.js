e3281555cfa1ce85c65472455f8c767fff8d1342
Josemaria Naranjo <josemnaranjoc@gmail.com>
2024-02-26 12:02:13 -0300

    creacion de pelicula y asociar a sesion correspondiente

--- server/controllers/pelicula.controller.js
+++ server/controllers/pelicula.controller.js
@@ -1,16 +1,27 @@
-import Pelicula from '../models/Pelicula.js'
+import { Pelicula } from "../models/Pelicula.js";
+import { Sesion } from "../models/Sesion.js";
 
-export const crearPelicula = async (req,res) => {
-    try {
-        const {titulo} = req.body;
-        const nuevaPelicula = await Pelicula.create({
-            titulo: titulo
-        })
-        res.json({mensaje: "Pelicula creada exitosamente", nuevaPelicula})
-    } catch (err) {
-        res.status(500).json({
-            mensaje:"Algo salió mal al crear una nueva película",
-            errores: err
-        })
-    }
-}
+export const crearPelicula = async (req, res) => {
+  try {
+    const { nombreSesion } = req.params;
+    const { titulo } = req.body;
+
+    //* obtengo informacion de la sesion, su id en particular
+    const sesion = await Sesion.findAll({
+      where: { nombreSesion: nombreSesion },
+    });
+    const sesionId = sesion[0].id;
+
+    //* creo película y asosio la sesion
+    const nuevaPelicula = await Pelicula.create({
+      titulo,
+      sesionId,
+    });
+    res.json({ mensaje: "Pelicula creada exitosamente", nuevaPelicula });
+  } catch (err) {
+    res.status(500).json({
+      mensaje: "Algo salió mal al crear una nueva película",
+      errores: err,
+    });
+  }
+};
