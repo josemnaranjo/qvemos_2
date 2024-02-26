1bc9ede5f76dcb38a4c5d8ce7ef21073749bf2be
Josemaria Naranjo <josemnaranjoc@gmail.com>
2024-02-26 12:22:56 -0300

    creacion de server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,36 @@
 import express from "express";
+import cors from "cors";
+import { sequelize } from "./config/mysql.config.js";
+
+//* importacion de rutas
+import usuarioRoutes from "./routes/usuario.routes.js";
+import sesionRoutes from "./routes/sesion.routes.js";
+import evaluacionRoutes from "./routes/evaluacion.routes.js";
+import peliculaRoutes from "./routes/pelicula.routes.js";
 
 const app = express();
+const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 
-const port = process.env.PORT || 5000;
+//* implementacion de rutas
+app.use(usuarioRoutes);
+app.use(sesionRoutes);
+app.use(evaluacionRoutes);
+app.use(peliculaRoutes);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+async function main() {
+  try {
+    await sequelize.sync();
+    console.log("Conexión exitosa a base de datos");
+    app.listen(port, () => {
+      console.log(`Conexón con el puerto ${port} establecida exitosamente`);
+    });
+  } catch (err) {
+    console.log("Error al establecer conexión con la base de datos");
+  }
+}
+
+main()
