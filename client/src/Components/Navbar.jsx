b236ba2e613f2d77a573e14621de75e8fe534eee
Josemaria Naranjo <josemnaranjoc@gmail.com>
2024-03-01 11:28:56 -0300

    navbar con conditional render y logout

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,25 +1,57 @@
 import { Link } from "react-router-dom";
+import { useUser } from "../context/userContext";
+import { logout } from "../api/usuario.services";
 
 const Navbar = () => {
-  return (
-    <>
-      <nav className="bg-primary font-grotesk flex items-center justify-between px-[8px] shadow-md">
-        <span className="font-bold text-lg text-secondary">
-          {" "}
-          <Link to={"/"}>Q-vemos</Link>
-        </span>
+  const { user, setUser } = useUser();
 
-        <div className="flex items-center gap-[27px]">
-          <span className="font-normal text-label text-secondary">
-            <Link to={"login"}>Login</Link>
+  const logoutUser = async () => {
+    const response = await logout();
+    const result = response.data.success
+    if (result) setUser(null);
+    else window.alert("Error. No hemos podido desloguear tu usuario");
+  };
+
+  const renderInfo = () => {
+    if (user) {
+      return (
+        <nav className="bg-primary font-grotesk flex items-center justify-between px-[8px] shadow-md">
+          <span className="font-bold text-lg text-secondary">
+            {" "}
+            <Link to={"/"}>Q-vemos</Link>
           </span>
-          <button className="font-normal text-label bg-secondary text-primary px-2 py-1 rounded-lg">
-            <Link to={"registrarse"}>registrarse</Link>
-          </button>
-        </div>
-      </nav>
-    </>
-  );
+          <div className="flex items-center gap-[27px]">
+            <span className="font-normal text-label text-secondary">
+              ¡Hola, {user}!
+            </span>
+            <button className="font-normal text-label bg-secondary text-primary px-2 py-1 rounded-lg" onClick={logoutUser}>
+              logout
+            </button>
+          </div>
+        </nav>
+      );
+    } else {
+      return (
+        <nav className="bg-primary font-grotesk flex items-center justify-between px-[8px] shadow-md">
+          <span className="font-bold text-lg text-secondary">
+            {" "}
+            <Link to={"/"}>Q-vemos</Link>
+          </span>
+
+          <div className="flex items-center gap-[27px]">
+            <span className="font-normal text-label text-secondary">
+              <Link to={"login"}>Login</Link>
+            </span>
+            <button className="font-normal text-label bg-secondary text-primary px-2 py-1 rounded-lg">
+              <Link to={"registrarse"}>registrarse</Link>
+            </button>
+          </div>
+        </nav>
+      );
+    }
+  };
+
+  return <>{renderInfo()}</>;
 };
 
 export default Navbar;
