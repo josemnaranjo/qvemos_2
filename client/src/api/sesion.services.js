40291f540407f42b15a59da19a01fe51ae06d9e7
Josemaria Naranjo <josemnaranjoc@gmail.com>
2024-03-06 15:43:52 -0300

    servicio para actualizar presencia de anfitrion en sala

--- client/src/api/sesion.services.js
+++ client/src/api/sesion.services.js
@@ -5,3 +5,8 @@ export const crearSesion = async (sesionData) =>
 
 export const obtenerSesion = async (nombreSesion) =>
   axios.get(`http://localhost:3000/api/obtener-sesion/${nombreSesion}`);
+
+export const actualizarPresenciaDeAnfitrion = async (nombreSesion) =>
+  axios.put(
+    `http://localhost:3000/api/actualizar-presencia-anfitrion/${nombreSesion}`
+  );
