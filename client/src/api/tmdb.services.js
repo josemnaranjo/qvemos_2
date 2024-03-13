b0597eefb9fe8a3c1f40aa010f7b0946e01e4ab3
Josemaria Naranjo <josemnaranjoc@gmail.com>
2024-03-13 15:36:16 -0300

    apikey en env

--- client/src/api/tmdb.services.js
+++ client/src/api/tmdb.services.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+const apiKey = import.meta.env.TMDB_API_KEY;
+
 export const getOneMovie = async (movieTitle) => {
   const res = await axios.get("https://api.themoviedb.org/3/search/movie", {
     params: { query: movieTitle, include_adult: true },
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYjA1MmE4NTAwMmY1MTE2YjBlMmQxMDJlYWEwNWMzNCIsInN1YiI6IjY1YjdhMGFjMTA4OWJhMDE3ZGY4MDlkNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.trrVd6pCrEfBP7wTorV9iLb-p4NRw223EFrZu_ETw_8`,
+      Authorization: `Bearer ${apiKey}`,
     },
   });
   return res.data;
