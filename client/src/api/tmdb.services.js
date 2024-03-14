8bc9e197b0034d06f7bf4f863d9864e56aaef123
Josemaria Naranjo <josemnaranjoc@gmail.com>
2024-03-14 10:31:49 -0300

    importacion de tmdb key

--- client/src/api/tmdb.services.js
+++ client/src/api/tmdb.services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiKey = import.meta.env.TMDB_API_KEY;
+const apiKey = import.meta.env.VITE_TMDB_API_KEY;
 
 export const getOneMovie = async (movieTitle) => {
   const res = await axios.get("https://api.themoviedb.org/3/search/movie", {
