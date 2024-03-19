4c6d07522e83a462e1125a5191064afb2522f31e
Josemaria Naranjo <josemnaranjoc@gmail.com>
2024-03-19 11:52:22 -0300

    ajuste de textos en pagina de detalle de pelicula

--- client/src/Views/MovieInfo.jsx
+++ client/src/Views/MovieInfo.jsx
@@ -6,19 +6,25 @@ const MovieInfo = () => {
   console.log(movie);
   return (
     <main className="flex flex-col items-center">
-      <article className="mt-[40px] w-[340px] h-[540px] bg-secondary rounded-xl text-primary flex flex-col items-center justify-around">
-        <h1 className="font-grotesk font-bold text-xxl">{movie.title}</h1>
-        <div className="flex flex-col gap-y-2">
-          <p className="font-grotesk font-bold text-body">Sinopsis</p>
-          <p className="w-[250px] font-grotesk font-light text-body">
-            {movie.overview}
-          </p>
+      <article className="mt-[40px] w-[340px] h-[680px] bg-secondary rounded-xl text-primary flex flex-col items-center">
+        <h1 className="mt-[40px] font-grotesk font-bold text-xl text-center">
+          {movie.title}
+        </h1>
+        <div className="mt-[20px] flex flex-col gap-y-2 px-10">
+          <h2 className="font-grotesk font-bold text-body">
+            Director :{" "}
+            <span className="font-grotesk font-light text-body">
+              Pedro Almodovar
+            </span>
+          </h2>
+          <h2 className="font-grotesk font-bold text-body">Sinopsis :</h2>
+          <p className="font-grotesk font-light text-body">{movie.overview}</p>
         </div>
-        <div className="w-[250px]">
-          <p className="font-grotesk font-bold text-body">Casting</p>
-          <p className="font-grotesk font-light text-body">Actor 1</p>
-          <p className="font-grotesk font-light text-body">Actor 2</p>
-        </div>
+        <img
+          src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+          alt={`${movie.title} photo`}
+          className="mt-[20px] rounded-xl"
+        />
       </article>
     </main>
   );
