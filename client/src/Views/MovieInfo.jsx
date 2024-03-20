ffd75140decab35e9fd3d5fcd945d2d473e773fc
Josemaria Naranjo <josemnaranjoc@gmail.com>
2024-03-20 10:32:36 -0300

    ajuste en diseño de movie info para mostrar mejor los elementos

--- client/src/Views/MovieInfo.jsx
+++ client/src/Views/MovieInfo.jsx
@@ -22,25 +22,25 @@ const MovieInfo = () => {
   }, []);
   return (
     <main className="flex flex-col items-center ">
-      <article className="mt-[40px] w-[345px] py-10  bg-secondary rounded-xl text-primary flex flex-col items-center relative">
+      <article className="mt-[40px] w-[365px] py-10  bg-secondary rounded-xl text-primary flex flex-col items-center relative">
         <Link to={`/buscador/${id}`}>
           <GoChevronLeft
             size={30}
             color="white"
-            className="mt-[20px] absolute -top-3 left-2 bg-primary rounded-2xl "
+            className="mt-[20px] absolute top-2 left-6 bg-primary rounded-2xl "
           />
         </Link>
-        <h1 className="mt-[45px] font-grotesk font-bold text-xl text-center">
+        <h1 className="mt-[45px] font-grotesk font-bold text-lg text-center">
           {movie.title}
         </h1>
-        <div className="mt-[20px] flex flex-col gap-y-2 px-10">
-          <h2 className="font-grotesk font-bold text-body">
+        <div className="mt-[20px] flex flex-col gap-y-2 px-6">
+          <h2 className="font-grotesk font-medium text-body">
             Director :{" "}
             <span className="font-grotesk font-light text-body">
               {directorName}
             </span>
           </h2>
-          <h2 className="font-grotesk font-bold text-body">Sinopsis :</h2>
+          <h2 className="font-grotesk font-medium text-body">Sinopsis :</h2>
           <p className="font-grotesk font-light text-body">{movie.overview}</p>
         </div>
         <img
