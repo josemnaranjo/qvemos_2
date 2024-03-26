b3f19c5896d48d202eb2066d9ee355b099abf60e
Josemaria Naranjo <josemnaranjoc@gmail.com>
2024-03-26 11:54:27 -0300

    feat: poster de peli ganadora en evaluacion y retoques en diseño

--- client/src/Views/Evaluacion.jsx
+++ client/src/Views/Evaluacion.jsx
@@ -2,40 +2,45 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { Formik, Form, Field } from "formik";
 import { resultadosVotacion, evaluarPelicula } from "../api/peliculas.services";
+import { getOneMovie } from "../api/tmdb.services";
 
 const Evaluacion = () => {
   const { id } = useParams();
   const navigate = useNavigate();
 
   const [peliculaGanadora, setPeliculaGanadora] = useState();
+  const [imgSrc, setImgSrc] = useState();
 
   const handleGetPeliculaGanadora = async () => {
     const response = await resultadosVotacion(id);
     setPeliculaGanadora(response.data);
+    const movieTitle = response.data.titulo;
+    const movieInfo = await getOneMovie(movieTitle);
+    setImgSrc(movieInfo.results[0].poster_path);
   };
 
   const handleSubmit = async (values) => {
     const titulo = peliculaGanadora.titulo;
     const evaluacion = { titulo: titulo, ...values.evaluacion };
     await evaluarPelicula(evaluacion);
-    navigate("/")
+    navigate("/");
   };
   useEffect(() => {
     handleGetPeliculaGanadora();
   }, []);
-  const imgSrc =
-    "https://www.movieposters.com/cdn/shop/files/THING_5d1279a2-2e38-4ca0-8cb5-91f9b30b6ebb_480x.progressive.jpg?v=1693491676";
   return (
     <main className="flex flex-col items-center">
       <article
-        className="mt-[46px] w-[342px] h-[98px] rounded-xl flex items-center justify-center"
-        style={{ backgroundImage: `url(${imgSrc})` }}
+        className="mt-[46px] w-[342px] h-[200px] rounded-xl flex items-center justify-center px-2"
+        style={{
+          backgroundImage: `url(https://image.tmdb.org/t/p/w400/${imgSrc})`,
+        }}
       >
-        <h1 className="font-grotesk font-bold text-xxl text-primary text-center">
+        <h1 className="font-grotesk font-bold text-xl text-primary font-outline-2 text-center">
           {peliculaGanadora?.titulo}
         </h1>
       </article>
-      <article className="mt-[10px] w-[342px] h-[566px] rounded-xl bg-secondary border-4 border-tertiary">
+      <article className="mt-[20px] w-[342px] rounded-xl bg-secondary border-4 border-tertiary">
         <Formik
           initialValues={{
             evaluacion: {
@@ -45,8 +50,8 @@ const Evaluacion = () => {
           }}
           onSubmit={(values) => handleSubmit(values)}
         >
-          <Form className="flex flex-col items-center">
-            <div className="mt-[80px] flex flex-col gap-y-3">
+          <Form className="flex flex-col items-center py-10">
+            <div className="flex flex-col gap-y-3">
               <label
                 htmlFor="evaluacion.comentario"
                 className="font-grotesk font-normal text-body text-primary"
@@ -64,7 +69,7 @@ const Evaluacion = () => {
                 htmlFor="evaluacion.puntuacion"
                 className="font-grotesk font-normal text-body text-primary"
               >
-                estrellitas
+                ¿Cuántas estrellitas?
               </label>
               <Field
                 as="select"
